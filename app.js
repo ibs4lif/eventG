e1675c20c2315bdfdfb772bd885db8347ed898aa
Ibrahima Sarr <ibrahimasarr88@gmail.com>
2016-04-12 21:18:13 -0400

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var routes = require('./routes/router');
+var routes = require('../routes/router');
 // var users = require('./routes/users');
 
 
