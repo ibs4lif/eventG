c2d515098980e1123ce686ccd2db24c44c9f008b
Ibrahima Sarr <ibrahimasarr88@gmail.com>
2016-04-13 18:47:03 -0400

    first commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname + "../../")));
+// app.use(express.static(path.join(__dirname + "../../")));
 
 app.use('/',routes);
 app.use('/contactlist', routes);
